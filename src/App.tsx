e99f10809eee18949ea89614c60203d7ef4c8e8a
ssul <younghwan.sul@wehuddling.com>
2022-05-20 13:31:40 +0900

    메인 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>티라미드 콘테스트를 위한 페이지 공사중</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://maintainker.github.io/tiramid/pointcounter/build/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          티라미드 티츄 카운터
         </a>
       </header>
     </div>
